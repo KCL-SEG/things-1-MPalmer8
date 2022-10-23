b2a08c83efb22ff140b0e23bca39ebfcd442a6b3
Matthew Palmer <matthewpalmer135@gmail.com>
2022-10-23 19:35:33 +0100

    Added integer field validation

--- things/models.py
+++ things/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
-    quantity = models.IntegerField()
-    
+    quantity = models.IntegerField(unique=False, validators=[MaxValueValidator(100), MinValueValidator(0)])
